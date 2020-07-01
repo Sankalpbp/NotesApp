090f94256f36988fbb1c865e3cba0fa9c63eaadb
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-01 23:25:17 +0530

    Add Note functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
  'use strict'
 
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 const chalk = require('chalk');
 const yargs = require('yargs');
 
@@ -26,8 +26,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ', argv.title);
-        console.log('Body: ', argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 });
 
--- notes.js
+++ notes.js
@@ -1,7 +1,54 @@
 'use strict'
 
+const fs = require('fs');
 const getNotes = function () {
     return "Your notes...";
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+    // get the array of JS objects
+    const notes = loadNotes();
+
+    const duplicateNotes = notes.filter(function (note) {
+        return (note.title === title);
+    });
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+    
+        saveNotes(notes);    
+        console.log('New note added.');
+    } else {
+        console.log('Note title taken.');
+    }
+}
+
+const saveNotes = function (notes) {
+
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function () {
+
+    try {
+        // get the JSON in a buffer(in the form of bytes)
+        const dataBuffer = fs.readFileSync('notes.json');
+        // convert the buffer bytes to string
+        const dataJSON = dataBuffer.toString();
+        // parsing the JSON data to a JS object
+        let JSObjectArray = JSON.parse(dataJSON);
+        return JSObjectArray;
+    } catch (error) {
+        // return an empty JS array
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
