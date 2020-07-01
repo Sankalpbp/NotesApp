17f3f4104328976aa56113ca82d3ac03a51c6307
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-01 20:37:19 +0530

    Command Added

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
  'use strict'
 
-const validator = require('validator');
-const fs = require('fs');
 const getNotes = require('./notes.js');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-console.log(getNotes());
-console.log(chalk.green('Success'));
-console.log(chalk.bold.red('Hey guys, how you doing'));
+yargs.version('1.1.0');
 
+// we will create different commands corresponding to 
+// the argument that is passed
+
+// create an add command
+yargs.command({
+    command: 'add',
+    description: 'Add a note',
+    handler: function() {
+        console.log('Adding a note...');
+    }
+});
+
+// create a remove command
+yargs.command({
+    command: 'remove',
+    description: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note...');
+    }
+});
+
+// create a list command
+yargs.command({
+    command: 'list',
+    description: 'List the notes',
+    handler: function () {
+        console.log('Listing the notes...');
+    }
+});
+
+console.log(yargs.argv);
